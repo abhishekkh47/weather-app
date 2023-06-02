9e382ce0666af72f56c1473280f153422c840804
Abhishek Khandelwal <abhishekkhandelwal47@gmail.com>
2023-06-02 10:06:50 +0530

    initail commit
    This is a weather app (frontend) made using JS

---
+++ app.js
@@ -0,0 +1,228 @@
+// ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+// edit option
+let editElement;
+let editFlag = false;
+let editID = "";
+
+// ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener("submit",addItem);
+// clear items
+clearBtn.addEventListener("click", clearItems);
+// load items (from local storage)
+window.addEventListener("DOMContentLoaded", setupItems);
+
+// const deleteBtn = document.querySelector(".delete-btn");
+// console.log(deleteBtn);   // will return null, coz we do not already have  delete btn, we add items dynamically, so initally it will be null
+
+// ****** FUNCTIONS **********
+function addItem(e){
+    e.preventDefault();
+    // console.log(grocery.value);  // print whatever we type in input
+    const value = grocery.value;
+    const id = new Date().getTime().toString();   // this will provide us with some unique number 
+    // console.log(id);
+    // we don't use such approach iin big projects
+    // its just for practice purpose so we dont need to import any external libs
+
+    // if (value)
+    // {
+    //     console.log("value is falsy");
+    // }
+
+    if (value && !editFlag )
+    {
+        // console.log("add item to the list");
+        createListItem(id, value);
+        // display alert
+        displayAlert("item added to the list", "success");
+        // show container
+        container.classList.add("show-container");
+        // add to local storage
+        addToLocalStorage(id, value);
+        // set back to default
+        setBackToDefault();
+    }
+    else if (value && editFlag )
+    {
+        // console.log("editing");
+        editElement.innerHTML = value;
+        displayAlert("value changed", "success");
+        // edit local storage
+        editLocalStorage(editID, value);
+        setBackToDefault();
+    }
+    else
+    {
+        // console.log('empty value');
+        displayAlert("please enter value", "danger");
+        
+    }
+}
+
+// display alert
+function displayAlert(text, action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    // remove alert
+    setTimeout(function(){
+        alert.textContent = "";
+        alert.classList.remove(`alert-${action}`);
+    }, 1000);
+}
+
+// clear  items function
+function clearItems(){
+    const items = document.querySelectorAll(".grocery-item");
+
+    if(items.length > 0)
+    {
+        items.forEach(function(item){
+            list.removeChild(item);
+        });
+    }
+    // now remove the "clear list" button also (after clearing the list)
+    container.classList.remove("show-container");
+    displayAlert("empty list", "danger");
+    setBackToDefault();
+    // remove items from local storage also
+    localStorage.removeItem("list");
+}
+
+// delete function
+function deleteItem(e) {
+    // console.log("item deleted");
+    const element = e.currentTarget.parentElement.parentElement;
+    const id = element.dataset.id;
+    list.removeChild(element);
+    if(list.children.length === 0)
+    {
+        container.classList.remove("show-container");
+    }
+    displayAlert("item removed", "danger");
+    setBackToDefault();
+    // remove from local storage
+    removeFromLocalStorage(id);
+}
+
+// edit function
+function editItem(e){
+    // console.log("edit item");
+    const element = e.currentTarget.parentElement.parentElement;
+    // set edit item
+    editElement = e.currentTarget.parentElement.previousElementSibling;
+    // set form value
+    grocery.value = editElement.innerHTML;
+    editFlag = true;
+    editID = element.dataset.id;
+    submitBtn.textContent = "edit";
+}
+
+// set back to default
+function setBackToDefault(){
+    // console.log("set back to default");
+    grocery.value = "";
+    editFlag = false;
+    editID = "";
+    submitBtn.textContent = "submit";
+}
+
+// ****** LOCAL STORAGE **********
+function addToLocalStorage(id, value){
+    const grocery = {id, value};     //ES6 syntax // also be written as {id:id, value:value} 
+    // console.log(grocery);
+    let items = getLocalStorage();                                     // ternary operator
+    // console.log(items);
+
+    items.push(grocery);
+    localStorage.setItem("list" , JSON.stringify(items));
+    // console.log("added to local storage");
+
+}
+
+function removeFromLocalStorage(id) {
+    let items = getLocalStorage();
+
+    items = items.filter(function (item) {
+        if(item.id !== id)
+        {
+            return item;
+        }
+    });
+    localStorage.setItem("list" , JSON.stringify(items));
+}
+
+function editLocalStorage(id, value) {
+    let items = getLocalStorage();
+    items = items.map(function (item) {
+        if(item.id === id)
+        {
+            item.value = value;
+        }
+        return item;
+    });
+    localStorage.setItem("list" , JSON.stringify(items));
+}
+
+function getLocalStorage(){
+    return localStorage.getItem("list") 
+        ? JSON.parse(localStorage.getItem('list')) 
+        : [] ;  
+}
+// localStorage API
+// setItem
+// getItem
+// removeItem
+// save as strings
+// localStorage.setItem("orange", JSON.stringify(["item1","item2"]));
+// const oranges = JSON.parse(localStorage.getItem("orange"));
+// console.log(oranges);   // return list of items => ["item1", "item2"]
+// localStorage.removeItem("orange"); 
+// we can check local storage in inspect element (dev tools) => applications
+
+
+// ****** SETUP ITEMS **********
+function setupItems() {
+    let items = getLocalStorage();
+    if(items.length > 0)
+    {
+        items.forEach(function(item){
+            createListItem(item.id, item.value)
+        });
+        container.classList.add("show-container");
+    }
+}
+
+function createListItem(id, value){
+    const element = document.createElement("article");
+        // add class
+        element.classList.add("grocery-item");
+        // add id
+        const attr = document.createAttribute("data-id");
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = `<p class="title">${value}</p>
+                            <div class="btn-container">
+                            <button type="button" class="edit-btn">
+                                <i class="fas fa-edit"></i>
+                            </button>
+                            <button type="button" class="delete-btn">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                            </div>`;
+        const deleteBtn = element.querySelector(".delete-btn");
+        const editBtn = element.querySelector(".edit-btn");
+        deleteBtn.addEventListener("click", deleteItem);
+        editBtn.addEventListener("click", editItem);
+        // append child
+        list.appendChild(element);
+}
